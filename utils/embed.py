8304142604c43384dfd778de92cc938a58a07411
ArsalanJdev <arsalanjdev@gmail.com>
2025-05-12 17:08:43 +0000

    added localization

--- utils/embed.py
+++ utils/embed.py
@@ -5,6 +5,7 @@
 import discord
 #Local file
 from utils.config_manager import *
+from utils.localization import Translator as T
 
 class EmbeddedList:
     def __init__(self, configManager: ConfigManager):
@@ -26,6 +27,7 @@ class EmbeddedList:
             colour=discord.Colour.from_str(self.configManager.getTicketClosureEmbedColor()) 
         )
         if self.configManager.getMessagesLog():
-            embed.set_footer(text="All messages you send here will be recorded.")
+            message = T().translate("ticket_closure.footer.log")
+            embed.set_footer(text=message)
         embed.set_author(name=f"{self.configManager.getTicketClosureAuthor()}")
         return embed
