187aaaa8720b453c2fa2c9b713e4864f4899e68d
ArsalanJdev <arsalanjdev@gmail.com>
2025-05-13 17:09:04 +0000

    fixed localization

--- utils/localization.py
+++ utils/localization.py
@@ -4,7 +4,7 @@
 
 
 """
-Handling the localization and multi-language support through dictionaries in json format in the ../lang directory.
+Handling the localization and multi-translator support through dictionaries in json format in the ../lang directory.
 """
 
 #External Library
@@ -12,14 +12,13 @@ import json
 import sys
 from typing import List, Dict
 #local file
-from pathlib import Path
 from paths_manager import PathsManager
 from utils.config_manager import ConfigManager
 
 
 class Translator:
     """
-    Handles the language of ui elements and strings. The class is a singleton to ensure there's only one instance in
+    Handles the translator of ui elements and strings. The class is a singleton to ensure there's only one instance in
     the app life-cycle.
     """
 
@@ -28,9 +27,9 @@ class Translator:
     def __new__(cls, lang_code="en"):
         """
         Ensures that only one instance of Translator is created (Singleton pattern).
-        Initializes the language code and loads the translations.
+        Initializes the translator code and loads the translations.
 
-        :param lang_code: The language code for the translation file (default is "en").
+        :param lang_code: The translator code for the translation file (default is "en").
         :return: A single instance of the Translator class.
         """
         if not cls._instance:
@@ -41,10 +40,10 @@ class Translator:
 
     def load_translations(self) -> Dict[str,str]:
         """
-        Loads all translation key-value pairs from the JSON dictionary for the specified language.
+        Loads all translation key-value pairs from the JSON dictionary for the specified translator.
 
         :return: A dictionary containing translation keys and their corresponding translated values.
-        :raises FileNotFoundError: If the translation file for the given language does not exist.
+        :raises FileNotFoundError: If the translation file for the given translator does not exist.
         """
         lang_code = self.lang_code
         log_enabled = ConfigManager.getConsoleLogEnabled()
@@ -54,16 +53,16 @@ class Translator:
                 return json.load(f)
         except FileNotFoundError:
             if log_enabled:
-                print(f"Language dictionary {lang_code}.json was not found. Will use the default language (English).")
+                print(f"Language dictionary {lang_code}.json was not found. Will use the default translator (English).")
                 try:
                     with open(f"{abs_path}/lang/en.json", "r", encoding="utf-8") as f:
                         return json.load(f)
                 except FileNotFoundError:
                     if log_enabled:
-                        print(f"Default language (lang/en.json) was not found. Terminating the program.")
+                        print(f"Default translator (lang/en.json) was not found. Terminating the program.")
                         sys.exit(1)
 
-    def translate(self,key,**kwargs) -> str:
+    def translate(self,key:str,**kwargs) -> str:
         """
         Translates a given key from the JSON dictionary and replaces any placeholders with values.
 
@@ -73,5 +72,3 @@ class Translator:
         """
         template = self.translations.get(key, f"[Missing:{key}]")
         return template.format(**kwargs)
-
-translator = Translator()
